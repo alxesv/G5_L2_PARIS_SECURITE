58b6d1e6a72a512f9bca0ae1dbca6b21eeee070b
Elisa Semedo <elisasemedo07@gmail.com>
2024-02-07 11:58:40 +0100

    login/register

--- backend/routes/Login.js
+++ backend/routes/Login.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
 const router = express.Router()
 
-router.post('/', (req, res, next) => {
-    res.send("Login")
-
+router.post('/', async (req, res, next) => {
+	try {
+		const {password, name} = req.body
+		const user = await User.findOne({name})
+		if(!user) return res.status(401).json({message: "Identifiant /Mot de passe invalide !"})
+		const isPasswordValid = await bcrypt.compare(password, user.password)
+		if(!isPasswordValid) return res.status(401).json({message: "Identifiant /Mot de passe invalide !"})
+		res.status(201).json({message: "Connexion réussi", user: {name:user.name}})
+		
+	} catch (error) {
+		console.log("Error : " , error)
+		res.status(500).json({message: error.message})
+	}
 })
 
 module.exports = router
