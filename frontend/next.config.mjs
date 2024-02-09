8d359e7d48d6f228573702e5ed126a9699a8bf87
kvong92 <vongkevin.c92@gmail.com>
2024-02-09 13:48:09 +0100

    add:csp-poweredbynext

---
+++ frontend/next.config.mjs
@@ -0,0 +1,32 @@
+const nextConfig = {
+  poweredByHeader: false,
+  async headers() {
+    const cspHeader = `
+      default-src 'self';
+      script-src 'self';
+      style-src 'self';
+      img-src 'self' blob: data:;
+      font-src 'self';
+      object-src 'none';
+      base-uri 'self';
+      form-action 'self';
+      frame-ancestors 'none';
+      block-all-mixed-content;
+      upgrade-insecure-requests;
+    `;
+    
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
