5b7e8c416ee8b9a906b655762706a6d44b8bdcd1
kvong92 <vongkevin.c92@gmail.com>
2024-02-09 12:48:07 +0100

    d

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,36 @@
 /** @type {import('next').NextConfig} */
+
+/** @type {import('next').NextConfig} */
+
+const cspHeader = `
+  default-src 'self';
+  script-src 'self';
+  style-src 'self';
+  img-src 'self' blob: data:;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  block-all-mixed-content;
+  upgrade-insecure-requests;
+`;
+
+const headersConfig = {
+  source: '/(.*)',
+  headers: [
+    {
+      key: 'Content-Security-Policy',
+      value: cspHeader.replace(/\n/g, ''),
+    },
+  ],
+};
+
 const nextConfig = {
     poweredByHeader: false,
+    async headers() {
+        return [headersConfig];
+    },
 };
 
 export default nextConfig;
